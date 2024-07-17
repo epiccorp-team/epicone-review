1c3a61c19da6c7db14ea3730fbde74dfdf4d9664
keykat <koin12000@naver.com>
2024-07-18 03:59:23 +0900

    [feature] get it -> get x로 변경

--- lib/get_it.dart
+++ lib/get_it.dart
@@ -1,22 +1,19 @@
 import 'package:data/repository/login_repository.impl.dart';
 import 'package:domain/repository/login_repository.dart';
 import 'package:domain/usecase/login_usecase.dart';
-import 'package:get_it/get_it.dart';
+import 'package:get/get.dart';
 import 'package:presentation/main/bindings/main_binding.dart';
 
 class ServiceLocator {
-  static var di = GetIt.instance;
 
   static void initializeApp() {
     // data
-    di.registerFactory<LoginRepository>(() => LoginRepositoryImpl());
+    Get.lazyPut<LoginRepository>(() => LoginRepositoryImpl());
 
     // domain
-    di.registerFactory<LoginUsecase>(() => di());
+    Get.lazyPut<LoginUsecase>(() => LoginUsecase(Get.find<LoginRepository>()));
 
     // presentation
     MainBinding().dependencies();
   }
-
-  GetIt get() => di();
 }
