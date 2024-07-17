b8a819058e06a2d3af230b3df48a12aa8cd4f9e7
keykat <koin12000@naver.com>
2024-07-17 21:35:55 +0900

    [feature] login repository impl 개발

--- packages/data/lib/repository/login_repository.impl.dart
+++ packages/data/lib/repository/login_repository.impl.dart
@@ -1,12 +1,20 @@
+import 'package:data/api/dio.dart';
+import 'package:data/api/login.api.dart';
+import 'package:data/dto/login.dto.dart';
+import 'package:data/mapper/mapper.dart';
 import 'package:domain/api_state.dart';
 import 'package:domain/entity/login.entity.dart';
+import 'package:domain/entity/login_error.entity.dart';
 import 'package:domain/repository/login_repository.dart';
+import 'package:flutter/foundation.dart';
 
 class LoginRepositoryImpl implements LoginRepository {
   @override
-  Future<ApiState<LoginEntity>>? login() {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<ApiState<LoginEntity>>? login() async {
+      return await compute((LoginDto loginDto) async {
+        return await loginDto.toDomain();
+      }, await LoginApi(LoginDio.loginDio()).getUer(),
+    );
   }
 
 }
