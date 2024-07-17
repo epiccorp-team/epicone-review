72de3c65c10351faef1a124555c3bd1d7a46d6b2
keykat <koin12000@naver.com>
2024-07-17 20:24:28 +0900

    [feature] login api 생성

--- packages/data/lib/api/login.api.dart
+++ packages/data/lib/api/login.api.dart
@@ -1,3 +1,15 @@
-class LoginApi {
-  
+import 'package:dio/dio.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:retrofit/retrofit.dart';
+
+import 'package:data/dto/login.dto.dart';
+
+part 'login.api.g.dart';
+
+@RestApi(baseUrl: 'https://dev-api.epicone.co.kr/api/v1/')
+abstract class LoginApi {
+  factory LoginApi(Dio dio, {String? baseUrl}) = _LoginApi;
+
+  @GET('/review/user')
+  Future<LoginDto> getUer();
 }
